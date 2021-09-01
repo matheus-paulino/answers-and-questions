8b39c2dc6bd3c562c32cdc9408dbcfeab92c88d6
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-08-31 23:29:52 -0300

    send form

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // Set engine EJS
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -11,6 +14,13 @@ app.get("/", (req, res) => {
 
 app.get('/question', (req, res) => {
     res.render('questions/question')
+})
+
+app.post('/question', (req, res) => {
+    let title = req.body.title
+    let description = req.body.description
+
+    res.send({title, description})
 })
 
 app.listen(8080, () => {
